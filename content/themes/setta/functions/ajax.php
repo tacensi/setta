82697062dc04a74c0316d16cd4dda18e46429ef3
Thiago Censi <tacensi@gmail.com>
2019-10-23 18:55:45 -0300

    ajax atividade

--- content/themes/setta/functions/ajax.php
+++ content/themes/setta/functions/ajax.php
@@ -63,3 +63,61 @@ function setta_get_modulo() {
 		)
 	);
 }
+
+function setta_get_atividades() {
+
+	// Erro genérico na falta de post
+	if ( ! $_POST['mod_id'] ) {
+		die(
+			json_encode(
+				array(
+					'body' => 'Não foi encontrada nenhuma atividade.',
+				)
+			)
+		);
+	}
+	$mod_id = intval( trim( $_POST[ 'mod_id' ] ) );
+
+	$modulo = get_post( $mod_id );
+	
+	
+	if ( ! $modulo ) {
+
+		die(
+			json_encode(
+				array(
+					'body' => 'Não foi encontrada nenhuma atividade.',
+				)
+			)
+		);
+
+	}
+
+	$page = $_POST['page'] ? $_POST['page'] : 1;
+
+	$atividade_query = new WP_Query(
+		array(
+			'post_type' => 'atividade',
+			'posts_per_page' => 2,
+			'post_status' => 'publish',
+			'paged' => $page,
+			'page' => $page,
+		)
+	);
+	
+	// Content usa ob para carregar de um template part.
+	ob_start();
+	get_template_part( 'partials/atividade', 'list' );
+	$body = ob_get_contents();
+	ob_end_clean();
+
+	die (
+		json_enconde(
+			array(
+				'title' => $title,
+				'body'  => $body,
+			)
+		)
+	);
+}
+
